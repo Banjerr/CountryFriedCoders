5a287b88a548ab2b78e0a31e54718ad0bf67215e
Ben Redden <benjamminredden@gmail.com>
2017-06-19 21:09:55 -0400

    got some filler in for the "about" section, touched up styles for the "social media" section

--- app/src/about/about.js
+++ app/src/about/about.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
-import Avatar from 'material-ui/Avatar';
+// import Avatar from 'material-ui/Avatar';
 import { SegmentedControl, SegmentedControlItem, Text, View } from 'react-desktop/macOs';
 import MotionMenu from 'react-motion-menu';
 import './about.css';
@@ -17,25 +17,36 @@ const SocialMedia = () => (
       type="circle"
       margin={120}
     >
-      <div className="button">
-        <Avatar src="https://en.gravatar.com/userimage/8689522/ab40580acfebad77a09c304579815be3.jpeg" />
+      <div className="button avatar-button">
+        <img alt="avatar" src="https://en.gravatar.com/userimage/8689522/ab40580acfebad77a09c304579815be3.jpeg" />
       </div>
       <div className="button">
-        <a target="_blank" href="https://www.reddit.com/user/banjerr/"><i className="fa fa-reddit-alien"></i></a>
+        <a rel="noopener noreferrer" target="_blank" href="https://www.reddit.com/user/banjerr/"><i className="fa fa-reddit-alien"></i></a>
       </div>
       <div className="button">
-        <a href="https://www.facebook.com/benjamminredden" target="_blank"><i className="fa fa-facebook"></i></a>
+        <a rel="noopener noreferrer" href="https://www.facebook.com/benjamminredden" target="_blank"><i className="fa fa-facebook"></i></a>
       </div>
       <div className="button">
-        <a target="_blank" href="https://twitter.com/_bengineer_"><i className="fa fa-twitter"></i></a>
+        <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/_bengineer_"><i className="fa fa-twitter"></i></a>
       </div>
       <div className="button">
-        <a href="https://www.instagram.com/banjerr/" target="_blank"><i className="fa fa-instagram"></i></a>
+        <a rel="noopener noreferrer" href="https://www.instagram.com/banjerr/" target="_blank"><i className="fa fa-instagram"></i></a>
       </div>
       <div className="button">
-        <a target="_blank" href="https://github.com/Banjerr"><i className="fa fa-github-alt"></i></a>
+        <a rel="noopener noreferrer" target="_blank" href="https://github.com/Banjerr"><i className="fa fa-github-alt"></i></a>
       </div>
     </MotionMenu>
+  </View>
+);
+
+const AboutContent = () => (
+  <View background="transparent"
+    horizontalAlignment="center"
+    verticalAlignment="center"
+    width="100%"
+    padding="160px 0 0"
+    height="100%">
+    <code>Father, old-time music enthusiast, amateur banjo builder, professional web dev (PHP, JS (Angular, ES6, Node, React), HTML/CSS, MySQL, Mongo.DB)</code>
   </View>
 );
 
@@ -56,7 +67,7 @@ class About extends Component {
   renderItems() {
     return [
       this.renderItem(1, 'Social Media', <Paper zDepth={2} className="paper"><SocialMedia /></Paper>),
-      this.renderItem(2, 'About', <Paper zDepth={2} className="paper"><Text>Content 2</Text></Paper>),
+      this.renderItem(2, 'About', <Paper zDepth={2} className="paper"><AboutContent /></Paper>),
       this.renderItem(3, 'Analytics', <Paper zDepth={2} className="paper"><figure><embed src="https://wakatime.com/share/@bengineer/c27a51aa-ae1a-43fb-b13c-ef23cc8f372b.svg"></embed></figure></Paper>)
     ];
   }
