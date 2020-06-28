ec759d33d206c591c985da5eabff2d23ffebd557
Ben Redden <benjamminredden@gmail.com>
2020-06-28 09:16:19 -0400

    adding checks for length and presence of value from contentful

--- src/pages/thoughts.js
+++ src/pages/thoughts.js
@@ -11,7 +11,7 @@ const createPostString = (post) => {
 
   post.content.forEach(dataPieces => {
     dataPieces.content.forEach(content => {
-      if (content.value.length) {
+      if (content && content.value && content.value.length) {
         postString += content.value + '\n'
       }
     })
